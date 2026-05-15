38ce4dffb1690c52eef5c033fd6d5ce1b86a9296
_cheptanui_ <cynthiaronoh24@gmail.com>
2026-05-15 14:13:38 +0300

    Spring Demo Configuration

---
+++ SpringDemo/src/main/java/com/cheptanui/App.java
@@ -0,0 +1,14 @@
+package com.cheptanui;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args){
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Alien alien1 = (Alien) context.getBean("alien");
+        alien1.code();
+    }
+}
